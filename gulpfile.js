a055d960a0b8cddea82eee1d7b5299fe2132e211
romanrurenko <rroman7@yandex.ru>
2018-08-27 17:20:35 +0700

    добавлен первый тест

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,8 @@ const imagemin = require(`gulp-imagemin`);
 const svgstore = require(`gulp-svgstore`);
 const rollup = require(`gulp-better-rollup`);
 const sourcemaps = require(`gulp-sourcemaps`);
+const mocha = require(`gulp-mocha`); // Добавим установленный gulp-mocha плагин
+const commonjs = require(`rollup-plugin-commonjs`); // Добавим плагин для работы с `commonjs` модулями
 
 gulp.task(`style`, () => {
   return gulp.src(`sass/style.scss`).
@@ -119,5 +121,15 @@ gulp.task(`scripts`, () => {
     .pipe(gulp.dest(`build/js`));
 });
 
-gulp.task(`test`, () => {
+gulp.task(`test`, function () {
+  return gulp
+    .src([`js/**/*.test.js`])
+    .pipe(rollup({
+      plugins: [
+        commonjs() // Сообщает Rollup, что модули можно загружать из node_modules
+      ]}, `cjs`)) // Выходной формат тестов — `CommonJS` модуль
+    .pipe(gulp.dest(`build/test`))
+    .pipe(mocha({
+      reporter: `spec` // Вид в котором я хочу отображать результаты тестирования
+    }));
 });
